a8bfa1efa56e43e3281e79ac18a21545ccae5d69
Till Hoffmann <till.hoffmann@enforge.de>
2019-10-06 14:32:30 +0200

    adding center class

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,5 @@
-//import "../styles.scss"
-//function Home() {
-//    return <div className="example">Welcome to Next.js!</div>
-//} 
-//export default Home
-
-
 import "../styles.scss"
 
-export default () => <div className="example">Hello World!</div>
+export default () => (
+  <div className="center">Welcome to Next.js!</div>
+)
